cc73d46f1591618c4276cd25c032713a8f83f9d6
Suning Yao <sy2940@nyu.edu>
2021-08-10 15:07:14 +0800

    :speech_balloon: Updating landing page and logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+// import React from 'react';
+// import clsx from 'clsx';
+// import Layout from '@theme/Layout';
+// import Link from '@docusaurus/Link';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import styles from './index.module.css';
+// import HomepageFeatures from '../components/HomepageFeatures';
+//
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             first, sth about me️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+//
+// export default function Home() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`web&blockchain...`}
+//       description="Blog posts by Suning Yao. About web development and blockchain. Official website: suningyao.com.">
+//       <HomepageHeader />
+//       <main>
+//         <HomepageFeatures />
+//       </main>
+//     </Layout>
+//   );
+// }
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            first, sth about me️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import React from 'react';
+import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`web&blockchain...`}
-      description="Blog posts by Suning Yao. About web development and blockchain. Official website: suningyao.com.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to='/docs/intro' />;
 }
